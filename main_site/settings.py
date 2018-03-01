b3fc544aa5675cb6e4ad628d70f475cf0b7e069b
Brennan Douglas <brennand97@gmail.com>
2018-02-28 21:44:03 -0800

    basic template layout setup

--- main_site/settings.py
+++ main_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'matchmaking.apps.MatchmakingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- matchmaking/views.py
+++ matchmaking/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+
+    message = 'Hello, world!'
+
+    return render(request, 'index.html', {
+        'message': message,
+    })
