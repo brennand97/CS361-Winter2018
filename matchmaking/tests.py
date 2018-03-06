a04bd013cf5683d481d9ad13909f6c596024a839
Brennan Douglas <brennand97@gmail.com>
2018-03-05 17:20:55 -0800

    began working on search function

--- matchmaking/tests.py
+++ matchmaking/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
 
+from .models import Location, Dog
+
 # Create your tests here.
+
+class SearchTestCase(TestCase):
+    
+    fixtures=['test_data']
+
+    def setUp(self):
+        #Location.objects.create(city="Corvallis", state="OR", zipcode=97333)
+        pass
+
+    def test_can_lookup_location(self):
+
+        location = Location.objects.get(zipcode=97333)
+        
+        self.assertEqual(location.city, "Corvallis")
+        self.assertEqual(location.state, "OR")
+
+    def test_dog_by_location_lookup(self):
+
+        location = Location.objects.get(zipcode=97333)
+        self.assertIsNotNone(location)
+
+        dogs = Dog.objects.filter(location=location)
+        self.assertIsNotNone(dogs)
+
+    def test_different_dog_location_lookup(self):
+
+        location_1 = Location.objects.get(zipcode=97333)
+        self.assertIsNotNone(location_1)
+
+        location_2 = Location.objects.get(zipcode=97331)
+        self.assertIsNotNone(location_2)
+
+        dogs_1 = Dog.objects.filter(location=location_1)
+        self.assertIsNotNone(dogs_1)
+
+        dogs_2 = Dog.objects.filter(location=location_2)
+        self.assertIsNotNone(dogs_2)
+
+        self.assertNotEqual(dogs_1, dogs_2)
--- matchmaking/views.py
+++ matchmaking/views.py
@@ -1,6 +1,21 @@
 from django.http import HttpResponse
 
 from django.shortcuts import render
+from django.core import serializers
+
+from .models import Dog, Location
+
+def reduce_json(data):
+    if isinstance(data, []):
+        r_data = []
+        for d in data:
+            if "model" in d:
+                r = d["fields"]
+                for f in r:
+    else:
+        if "model" in data:
+            data = data["fields"]
+            
 
 # Create your views here.
 def index(request):
@@ -10,3 +25,14 @@ def index(request):
     return render(request, 'index.html', {
         'message': message,
     })
+
+def search(request, zipcode):
+    
+    location = Location.objects.get(zipcode=zipcode)
+    dogs = Dog.objects.filter(location=location)
+   
+    data = serializers.serialize("json", dogs)
+    
+
+    return HttpResponse(data, content_type='application/json')
+
