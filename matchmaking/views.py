335f220378c5abe44d328244c25cb6f272f41611
Brennan Douglas <brennand97@gmail.com>
2018-03-05 19:24:20 -0800

    removed reduce method

--- matchmaking/views.py
+++ matchmaking/views.py
@@ -5,18 +5,6 @@ from django.core import serializers
 
 from .models import Dog, Location
 
-def reduce_json(data):
-    if isinstance(data, []):
-        r_data = []
-        for d in data:
-            if "model" in d:
-                r = d["fields"]
-                for f in r:
-    else:
-        if "model" in data:
-            data = data["fields"]
-            
-
 # Create your views here.
 def index(request):
 
@@ -32,7 +20,6 @@ def search(request, zipcode):
     dogs = Dog.objects.filter(location=location)
    
     data = serializers.serialize("json", dogs)
-    
 
     return HttpResponse(data, content_type='application/json')
 
