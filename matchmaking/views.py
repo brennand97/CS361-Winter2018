5e6618c53b1256a25649101a8b77f19e8caaaa60
Riley <harrisri@oregonstate.edu>
2018-03-06 21:38:15 -0800

    used Brennan's helper functions to clean up JSON response

--- matchmaking/views.py
+++ matchmaking/views.py
@@ -1,9 +1,41 @@
 from django.http import HttpResponse
-
 from django.shortcuts import render
 from django.core import serializers
-
 from .models import Dog, Location
+
+import json
+
+# Helper Functions
+"""
+This function reduces the nasty json that the Django serailizer produces
+to objects that only contain the fields.
+Example usage:
+dogs = Dog.objects.filter(location__in=locations)
+data = serializers.serialize("json", dogs)
+data = json.dumps(reduce_json(json.loads(data)))
+"""
+
+def reduce_json(data):
+    if isinstance(data, (list,tuple)):
+        o_data = []
+        for d in data:
+            o_data.append(reduce_json(d))
+        return o_data
+    else:
+        if isinstance(data, dict):
+            if "model" in data:
+                data = data["fields"]
+            for f in data:
+                data[f] = reduce_json(data[f])
+        return data
+
+
+"""
+This is a helper function for reduce_json.
+"""
+def queryset_to_json(qs):
+    return json.dumps(reduce_json(json.loads(serializers.serialize("json", qs))))
+
 
 # Create your views here.
 def index(request):
@@ -15,9 +47,8 @@ def index(request):
     })
 
 def search(request, zipcode):
-    
     locations = Location.objects.filter(zipcode=zipcode)
     dogs = Dog.objects.filter(location__in=locations)
-    #data = serializers.serialize("json", dogs)
-    #return HttpResponse(data, content_type='application/json')
-    return HttpResponse(list(dogs), content_type='application/json')
+    data = serializers.serialize("json", dogs)
+    data = json.dumps(reduce_json(json.loads(data)))
+    return HttpResponse(data, content_type='application/json')
