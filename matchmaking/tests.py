d5143fd313018023596dc79042400b454b5403a6
kilgorep <kilgorep@oregonstate.edu>
2018-03-10 13:02:01 -0600

    Additional backend tests added
    Tests added for sex, color, and kid friendliness filters

--- matchmaking/tests.py
+++ matchmaking/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from .models import Location, Dog
+from .models import Location, Dog, PhysicalQualities, PersonalityQualities
 
 # Create your tests here.
 
@@ -56,3 +56,57 @@ class SearchTestCase(TestCase):
         self.assertIsNotNone(dogs_2)
 
         self.assertNotEqual(dogs_1, dogs_2)
+
+    def test_sex_filter(self):
+        # DB contains Rex as the only dog in 97333, and Rex is male
+        location = Location.objects.get(zipcode=97333)
+        self.assertIsNotNone(location)
+
+        dogs_1 = Dog.objects.filter(location=location)
+        self.assertIsNotNone(dogs_1)
+
+        dogs_2 = dogs_1.filter(sex='f')
+        self.assertNotEqual(list(dogs_1), list(dogs_2))
+
+        dogs_3 = dogs_1.filter(sex='m')
+        self.assertEqual(list(dogs_1), list(dogs_3))
+
+    def test_color_filter(self):
+        # DB contains Rex as the only dog in 97333, and Rex is male
+        location = Location.objects.get(zipcode=97333)
+        self.assertIsNotNone(location)
+
+        all_physicals = PhysicalQualities.objects.all()
+        d_color1 = all_physicals.filter(color="Yellow")
+        self.assertTrue(list(d_color1))
+        d_color2 = all_physicals.filter(color="Light Brown")
+        self.assertTrue(list(d_color2))
+
+        # Should return empty set since there are no yellow dogs in the zipcode
+        dogs_1 = Dog.objects.filter(location=location).filter(physical__in=d_color1)
+        self.assertFalse(list(dogs_1))
+
+        # Should return at least 1 dog in the zipcode
+        dogs_2 = Dog.objects.filter(location=location).filter(physical__in=d_color2)
+        self.assertTrue(list(dogs_2))
+
+    def test_kid_friendly_filter(self):
+        # DB has two dogs in 97331, one is kid friendly and one is not
+
+        location = Location.objects.get(zipcode=97331)
+        self.assertIsNotNone(location)
+
+        kf1 = PersonalityQualities.objects.filter(kid_friendly=1)
+        self.assertTrue(list(kf1)) # True if a PersonalityQualities object was returned
+
+        kf0 = PersonalityQualities.objects.filter(kid_friendly=0)
+        self.assertTrue(list(kf0)) # True if a PersonalityQualities object was returned
+
+        dogs_0 = Dog.objects.filter(location=location).filter(personality__in=kf0)
+        self.assertTrue(list(dogs_0)) # True if a Dog object was returned
+
+        dogs_1 = Dog.objects.filter(location=location).filter(personality__in=kf1)
+        self.assertTrue(list(dogs_1)) # True if a Dog object was returned
+
+        # Check that the two filtered results are not equal to one another
+        self.assertNotEqual(list(dogs_0), list(dogs_1))
