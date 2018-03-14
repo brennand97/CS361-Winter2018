35ceeceffbe48093e02c8bc829d728e2bf7a6cc0
Brennan Douglas <brennand97@gmail.com>
2018-03-13 20:03:58 -0700

    fixed syntax error

--- matchmaking/urls.py
+++ matchmaking/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path(r'', views.index, name='index'),
     path(r'add_dog/', views.add_dog, name='add_dog'),
     path(r'search/<int:zipcode>/', views.search, name='search'),
-    path(r'view_listings/', views.view_listings, name='view_listings')
-    path(r'shelter_dogs/<str:shelter>/', views.shelter_dogs, name='shelter_dogs')
+    path(r'view_listings/', views.view_listings, name='view_listings'),
+    path(r'shelter_dogs/<str:shelter>/', views.shelter_dogs, name='shelter_dogs'),
 ]
