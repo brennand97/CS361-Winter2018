7159f41106557a2f6dcc4fabf7bcace9230fcf8b
Riley <harrisri@oregonstate.edu>
2018-03-13 21:52:44 -0700

    added url for add_dog function

--- matchmaking/urls.py
+++ matchmaking/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path(r'', views.index, name='index'),
     path(r'add_dog/', views.add_dog, name='add_dog'),
     path(r'search/<int:zipcode>/', views.search, name='search'),
+    path(r'add_dog_to_db/',views.add_dog_to_db, name='add_dog_to_db')
 ]
